fa99f8fa659d5432218124a1b2b7077cd72533bd
HP <HP@DESKTOP-NRC>
2022-10-02 16:09:54 +0530

    first Commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Test1 
+{
+  public static void main(String[] args)
+  {
+	System.out.println("Hello");
+	m1();
+}
+  public static void m1()
+  {
+	  System.out.println("Hi");
+  }
+}
